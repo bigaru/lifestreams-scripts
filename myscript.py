f3b1dcf35d3a42aa39b41c36391665f2487a4574
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2025-03-16 22:55:12 +0100

    process fitabase data

--- myscript.py
+++ myscript.py
@@ -1,22 +1,38 @@
 import numpy as np
 import pandas as pd
-import psycopg2
+import psycopg
 import io
+from datetime import datetime, timedelta
 
 
-mean = 0  # Mean (μ)
-std_dev = 1  # Standard deviation (σ)
-sample_size = 10000
+def transform_date(date_str):
+    date_obj = datetime.strptime(date_str, "%m/%d/%Y %I:%M:%S %p")
 
-data = np.random.normal(loc=mean, scale=std_dev, size=sample_size)
+    # Shift to '1/04/2025 12:00:00'
+    new_date = date_obj + timedelta(days=3222)
+    return new_date.timestamp()
 
-time = pd.date_range(start="2025-01-01", periods=sample_size, freq="D")
-time = time.strftime("%Y-%m-%d %H:%M:%S")
 
-df = pd.DataFrame({"subject_id": 1, "data_type": 1, "datetime": time, "reading": data})
+# mean = 0  # Mean (μ)
+# std_dev = 1  # Standard deviation (σ)
+# sample_size = 10000
 
-print(df.head(10))
+# data = np.random.normal(loc=mean, scale=std_dev, size=sample_size)
 
+# time = pd.date_range(start="2025-01-01", periods=sample_size, freq="D")
+# time = time.strftime("%Y-%m-%d %H:%M:%S")
+
+# df = pd.DataFrame({"subject_id": 1, "data_type": 1, "datetime": time, "reading": data})
+# print(df.head(10))
+
+df_steps = pd.read_csv("./fitabase/data/hourlySteps_merged.csv")
+df_steps = df_steps.rename(
+    columns={"Id": "subject_id", "ActivityHour": "datetime", "StepTotal": "reading"}
+)
+df_steps["data_type"] = 2
+df_steps["datetime"] = df_steps["datetime"].map(transform_date)
+
+print(df_steps.head(10))
 
 db_params = {
     "dbname": "examples",
@@ -26,43 +42,34 @@ db_params = {
     "port": 5432,
 }
 
-cursor = None
-conn = None
-
 try:
-    conn = psycopg2.connect(**db_params)
-    cursor = conn.cursor()
-    conn.autocommit = True
+    with psycopg.connect(**db_params) as conn:
+        with conn.cursor() as cursor:
+            conn.autocommit = True
 
-    create_table_query = """
-    CREATE TABLE IF NOT EXISTS readings (
-        id SERIAL PRIMARY KEY,
-        subject_id INT,
-        data_type INT,
-        datetime TIMESTAMP,
-        reading DOUBLE PRECISION
-    );
-    """
+            cursor.execute("DROP TABLE IF EXISTS readings")
 
-    cursor.execute(create_table_query)
+            create_table_query = """
+            CREATE TABLE IF NOT EXISTS readings (
+                id SERIAL PRIMARY KEY,
+                subject_id BIGINT,
+                data_type INT,
+                datetime DOUBLE PRECISION,
+                reading DOUBLE PRECISION
+            );
+            """
 
-    buffer = io.StringIO()
-    df.to_csv(buffer, index=False, header=False)
-    buffer.seek(0)
-    cursor.copy_from(
-        file=buffer,
-        table="readings",
-        columns=("subject_id", "data_type", "datetime", "reading"),
-        sep=",",
-        null="",
-    )
+            cursor.execute(create_table_query)
 
+            buffer = io.StringIO()
+            df_steps.to_csv(buffer, index=False, header=False)
+            buffer.seek(0)
+
+            with cursor.copy(
+                "COPY readings (subject_id, datetime, reading, data_type) FROM STDIN WITH CSV"
+            ) as copy:
+                while data := buffer.read(512):
+                    copy.write(data)
 
 except Exception as e:
     print(f"An error occurred: {e}")
-
-finally:
-    if cursor:
-        cursor.close()
-    if conn:
-        conn.close()
